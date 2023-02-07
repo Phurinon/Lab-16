91248d42fbf1eed10293a2c30ca8ea51c626a23b
Phurinon <follk.5834@gmail.com>
2023-02-07 16:32:11 +0700

    lab16_5 complete

--- lab16_5.cpp
+++ lab16_5.cpp
@@ -3,7 +3,7 @@
 #include <ctime>
 using namespace std;
 
-void shuffle(int &,int &, int &, int &); //Modify input arguments to pointer 
+void shuffle(int *,int *, int *, int *); //Modify input arguments to pointer 
 
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
@@ -11,7 +11,7 @@ int main(){
 	srand(time(0));	
 	
 	for(int i = 0;i < 10;i++){
-	    shuffle(a,b,c,d); //Modify input arguments to pointer 
+	    shuffle(&a,&b,&c,&d); //Modify input arguments to pointer 
 	    cout << a << " " << b << " " << c << " " << d << "\n";
 	}
 	
@@ -19,3 +19,34 @@ int main(){
 }
 
 //Write definition of shuffle() using pointer here 
+void shuffle(int *a,int *b,int *c,int *d){
+	int t = rand()%3+1;
+	int w,x,y,z;
+	w=*a;
+	x=*b;
+	y=*c;
+	z=*d;
+	switch (t)
+	{
+	case 1:
+		*a=x;
+		*b=y;
+		*c=z;
+		*d=w;
+		break;
+	case 2:
+		*a=y;
+		*b=z;
+		*c=w;
+		*d=x;
+		break;
+	case 3:
+		*a=z;
+		*b=w;
+		*c=x;
+		*d=y;
+		break;
+	default:
+		break;
+	}
+}
